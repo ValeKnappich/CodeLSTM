e6f010dc00d78e9e0fd690cf82b0a3956ead4e3d
ValeKnappich <valentin.knappich@web.de>
2021-06-27 15:46:38 +0000

    .

--- milestone3/Predict.py
+++ milestone3/Predict.py
@@ -7,7 +7,7 @@ import torch
 from torch.utils.data.dataloader import DataLoader
 from tqdm import tqdm
 
-from data import combine_batch, load_data, load_multiple, token_index_to_char_index
+from data import combine_batch, load_data, load_multiple, token_index_to_char_index, FIX_TYPES
 from Train import CodeLSTM      # 'useless' import is needed for torch to load model
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -16,7 +16,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     '--model', help="Path of trained model.", default="model.pth")
 parser.add_argument(
-    '--source', help="Folder path of all test files.", default="dataset")
+    '--source', help="Folder path of all test files.", default="../dataset/training_0.json")
 parser.add_argument(
     '--destination', help="Path to output json file of extracted predictions.", default="predictions.json")
 
@@ -31,23 +31,38 @@ def predict(model: torch.nn.Module, test_files: str):
         raise FileNotFoundError(f"File or directory {test_files.absolute()} was not found")
 
     dl = DataLoader(train_ds, batch_size=32, shuffle=False, collate_fn=combine_batch)
-    metas, char_preds = [], []
-    for input_ids, tokens, labels, meta_data, wrong_code in tqdm(dl):
-        labels    = labels.to(DEVICE)
-        input_ids = input_ids.to(DEVICE)
-        logits    = model(input_ids)
-        preds_    = logits.argmax(dim=1)
+    metas, preds = [], []
+    for input_ids, tokens, fix_location, fix_type, fix_token, meta_data, wrong_code in tqdm(dl):
+        input_ids      = input_ids.to(DEVICE)
+        fix_location   = fix_location.to(DEVICE)
+        fix_type       = fix_type.to(DEVICE)
+        fix_token_mask = fix_token != -1        # filter out the instances where there is no token to predict (delete type)
+        fix_token      = fix_token[fix_token_mask].to(DEVICE)
+
+        logits_location, logits_type, logits_token = model(input_ids)
+        location_pred = logits_location.argmax(dim=1)
+        type_pred     = logits_type.argmax(dim=1)
+        token_pred    = logits_token.argmax(dim=1)
 
         metas.extend(meta_data)
-        char_preds.extend([
-            token_index_to_char_index(code, tokens_, pred, meta)
-            for code, tokens_, pred, meta in zip(wrong_code, tokens, preds_, meta_data)
+        preds.extend([{
+            "predicted_location": token_index_to_char_index(code, tokens_, loc, meta),
+            "predicted_type": FIX_TYPES[typ],
+            "predicted_token": model.vocab[tok],
+            "predicted_code": fix_code()
+            "metadata": {
+                "id": meta["id"],
+                "file": meta["file"]
+            }
+        } for loc, typ, tok, tokens_, code, meta
+          in zip(location_pred, type_pred, token_pred, tokens, wrong_code, meta_data)
         ])
 	
-    return [
-        {"predicted_location": pred, "metadata": meta}
-        for pred, meta in zip(char_preds, metas)
-    ]
+    for pred in preds:
+        if pred["predicted_type"] == "delete":
+            del pred["predicted_token"]
+
+    return preds
 
 
 def load_model(source: str):
@@ -57,11 +72,6 @@ def load_model(source: str):
 
 
 def write_predictions(destination: str, predictions: List[dict]):
-    n_correct = sum([
-        1 if prediction["predicted_location"] == prediction["metadata"]["fix_location"] else 0
-        for prediction in predictions
-    ])
-    print(f"Test Accuracy: {n_correct / len(predictions)}")
     json.dump(predictions, open(destination, "w"), indent=2)
 
 
@@ -77,6 +87,6 @@ if __name__ == "__main__":
     predictions = predict(model, args.source)
 
 	# write predictions to file
-    write_predictions(args.destination,predictions)
+    write_predictions(args.destination, predictions)
 
 	
